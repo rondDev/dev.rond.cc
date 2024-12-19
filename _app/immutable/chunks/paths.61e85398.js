950aae2bf5073d1fe9fd3fed4be98f5861c2ca21
rondDev <contact@rond.cc>
2024-12-19 18:20:31 +0100

    Updates

--- _app/immutable/chunks/paths.61e85398.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_1m3h036)==null?void 0:s.base)??"/dev.rond.cc";var e;const t=((e=globalThis.__sveltekit_1m3h036)==null?void 0:e.assets)??a;export{t as a,a as b};
